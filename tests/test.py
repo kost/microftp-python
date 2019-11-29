4d600198670926a88d6560414b88e0f263e1cf80
Vlatko Kosturjak <kost@diverto.hr>
2019-11-29 18:20:04 +0100

    Initial import

---
+++ tests/test.py
@@ -0,0 +1,8 @@
+from microftp import microFTP
+
+def test_class_instant():
+    '''
+    make sure class be instantize
+    '''
+
+    ''' assert new(somePython.fahrToKelv(32) == 273.15, 'incorrect freezing point!' '''
